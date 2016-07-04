d3a58ccddb3a8b49322dfb3d3d3c354883df2f41
Adan Santos <santos.adaan@gmail.com>
2016-07-04 17:41:49 -0300

    Primeiro Commit

---
+++ app/Events/Event.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Marketplace\Events;
+
+abstract class Event
+{
+    //
+}
---
+++ app/Http/Requests/Request.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Marketplace\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+abstract class Request extends FormRequest
+{
+    //
+}
